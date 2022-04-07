975c259a99590acc828837a6c876d2a9551cd466
ung <ung6039@naver.com>
2022-04-07 21:25:06 +0900

    team_add

---
+++ TeamProject_temp/src/test/java/com/spring/team/service/UserDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.spring.team.service;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+// ?���? 권한?�� ?��?�� 경우 ?��?��
+public class UserDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+		System.out.println("<<< UserDeniedHandler - handle 진입 >>>");
+		System.out.println("sessionId : " + request.getSession().getAttribute("customerID"));
+		
+		request.setAttribute("errMsg", "�?리자�? ?��근할 ?�� ?��?�� ?��?���??��?��?��.");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/common/accessDenied.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+}
